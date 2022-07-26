7ee459bfe2e544c8b43cf68a1d97def17fe8fe94
Huiling Tan <huilingt@hotmail.com>
2022-07-26 16:20:18 +0800

    regenerate category model and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_074250) do
+ActiveRecord::Schema.define(version: 2022_07_26_081324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
